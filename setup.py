b57d20cfd8c947036acb90e4c0b5ba90521c01cb
van Schie Alexander <alexander.vanschie@fhgr.ch>
2020-08-10 13:05:18 +0200

    Fixing paths in order to testrun on windows

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import os
+from pathlib import Path
 
 try:
     from orbis_eval.core.orbis_setup import OrbisSetupBaseClass
-    OrbisSetupBaseClass().run(os.path.dirname(os.path.realpath(__file__)))
+    OrbisSetupBaseClass().run(Path(__file__).parent)
 except Exception as exception:
     print("Orbis not found. Please install Orbis first.")
     print(f"({exception})")
